f0f00973daa4fb8ecc7242f4eaa84d9f2027110a
Your Name <n.t.quoctoan@gmail.com>
2023-05-07 17:34:46 +0700

    init

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // String type
+        // System.out.println = "sout" + tab
+        // Check error = F5
+        // Run not check error = Ctrl + F5
+        String myName = "Nguyen tran quoc Toan";
+        
+        System.out.println("My name is"+myName);
+
+        System.out.println(myName.toUpperCase()); 
+        System.out.println(myName.toLowerCase());
+        System.out.println("Length of string (contain spacing)"+myName.length());
+        // System.out.println(myName.toTitleCase()); Java version > 11
+
+        System.out.println("Hello, First Java!");
+        System.out.println("Tao là Toàn nè");
+        
+    }
+}
