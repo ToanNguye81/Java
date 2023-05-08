103eb351760900c1bda4af68466cd323046f6b2e
Your Name <n.t.quoctoan@gmail.com>
2023-05-08 20:37:11 +0700

    array and for

---
+++ src/com/learn2/javabasic/s10/ForArray.java
@@ -0,0 +1,22 @@
+package com.learn2.javabasic.s10;
+
+import java.util.Random;
+
+public class ForArray {
+    public static void newArrayList() {
+        Random random = new Random();
+        Integer[] arrayList = new Integer[10];
+        // create random List
+        for (int i = 0; i < 10; i++) {
+            arrayList[i] = Integer.valueOf(random.nextInt());
+        }
+
+        for (int i = 0; i < arrayList.length; i++) {
+            System.out.println(arrayList[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        newArrayList();
+    }
+}
