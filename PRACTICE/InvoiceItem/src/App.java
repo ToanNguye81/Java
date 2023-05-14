e5e53f319a96bfd88b51f2c2638b346f9fe55a0f
Your Name <n.t.quoctoan@gmail.com>
2023-05-14 17:42:13 +0700

    Account class

--- PRACTICE/InvoiceItem/src/App.java
+++ PRACTICE/InvoiceItem/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Invoice item
         InvoiceItem invoiceItem = new InvoiceItem(null, null, 0, 0);
         InvoiceItem invoiceItem2 = new InvoiceItem("4", "hẹn mai gặp", 3, 32);
         System.out.println(invoiceItem);
@@ -8,6 +8,19 @@ public class App {
         invoiceItem2.setUnitPrice(9);
         System.out.println(invoiceItem2);
         System.out.println(invoiceItem2.getTotal());
+
+        // Account
+        Account account = new Account(null, null);
+        Account account1 = new Account("232", "Đại gia");
+        Account account2 = new Account("232", "Đại gia", 5000);
+
+        account2.transferTo(45, account1);
+        account1.credit(12222);
+        account2.debit(1276);
+        System.out.println(account);
+        System.out.println(account1);
+        System.out.println(account2);
+
     }
 
 }
