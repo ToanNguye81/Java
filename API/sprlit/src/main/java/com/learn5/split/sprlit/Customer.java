873b1f56a410f82e3b7868336da1bd07a9a67bb5
Your Name <n.t.quoctoan@gmail.com>
2023-05-16 16:46:36 +0700

    customer-invoice

---
+++ API/sprlit/src/main/java/com/learn5/split/sprlit/Customer.java
@@ -0,0 +1,36 @@
+package com.learn5.split.sprlit;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private int discount;
+
+    public Customer(int id, String name, int discount) {
+        this.id = id;
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "Customer[id=" + id + ",name=" + name + ",discount=" + discount + "]";
+    }
+}
