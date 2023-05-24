60e9867f089199b2133bf787d64eced72b5dce74
Your Name <n.t.quoctoan@gmail.com>
2023-05-24 22:10:03 +0700

    district

---
+++ API/connect/src/main/java/com/learn/first/restapi/regions/model/CDistrict.java
@@ -0,0 +1,77 @@
+package com.learn.first.restapi.regions.model;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "districts")
+public class CDistrict {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "district_id", unique = true)
+    private int district_id;
+
+    @Column(name = "district_name")
+    private String districtName;
+
+    @Column(name = "district_prefix", unique = true)
+    private String districtPrefix;
+
+    @ManyToOne
+    @JoinColumn(name = "province_id")
+    private CProvince province;
+
+    @OneToMany(mappedBy = "district", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<CWard> wards;
+
+    public CDistrict(int district_id, String districtName, String districtPrefix) {
+        this.district_id = district_id;
+        this.districtName = districtName;
+        this.districtPrefix = districtPrefix;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public void setDistrictPrefix(String districtPrefix) {
+        this.districtPrefix = districtPrefix;
+    }
+
+    public void setProvince(CProvince province) {
+        this.province = province;
+    }
+
+    public void setWards(Set<CWard> wards) {
+        this.wards = wards;
+    }
+
+    public void setWard(Set<CWard> wards) {
+        this.wards = wards;
+    }
+
+    public Set<CWard> getWards() {
+        return wards;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public String getDistrictPrefix() {
+        return districtPrefix;
+    }
+
+    public CProvince getProvince() {
+        return province;
+    }
+
+    public Set<CWard> getWard() {
+        return wards;
+    }
+}
---
+++ API/connect/src/main/java/com/learn/first/restapi/regions/repository/IDistrictRepository.java
@@ -0,0 +1,9 @@
+package com.learn.first.restapi.regions.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learn.first.restapi.regions.model.CDistrict;
+
+public interface IDistrictRepository extends JpaRepository<CDistrict, Long> {
+
+}
