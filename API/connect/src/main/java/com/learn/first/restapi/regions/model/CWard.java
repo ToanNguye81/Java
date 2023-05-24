5a3fb61c38dbab54dbcf87e4e03d20a39177364d
Your Name <n.t.quoctoan@gmail.com>
2023-05-24 22:10:30 +0700

    ward

---
+++ API/connect/src/main/java/com/learn/first/restapi/regions/model/CWard.java
@@ -0,0 +1,61 @@
+package com.learn.first.restapi.regions.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ward")
+public class CWard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ward_id", unique = true)
+    private int ward_id;
+
+    @Column(name = "ward_name")
+    private String wardName;
+
+    @Column(name = "ward_prefix")
+    private String wardPrefix;
+
+    @ManyToOne
+    @JoinColumn(name = "district_id")
+    private CDistrict district;
+
+    public CWard(int ward_id, String wardName, String wardPrefix) {
+        this.wardName = wardName;
+        this.wardPrefix = wardPrefix;
+    }
+
+    public CDistrict getDistrict() {
+        return district;
+    }
+
+    public String getWardName() {
+        return wardName;
+    }
+
+    public String getWardPrefix() {
+        return wardPrefix;
+    }
+
+    public int getWard_id() {
+        return ward_id;
+    }
+
+    public void setWard_id(int ward_id) {
+        this.ward_id = ward_id;
+    }
+
+    public void setDistrict(CDistrict district) {
+        this.district = district;
+    }
+
+    public void setWardName(String wardName) {
+        this.wardName = wardName;
+    }
+
+    public void setWardPrefix(String wardPrefix) {
+        this.wardPrefix = wardPrefix;
+    }
+
+}
