2d7584f3b4945b2c054f195f49cec8d12b2fabaa
Your Name <n.t.quoctoan@gmail.com>
2023-05-29 17:06:36 +0700

    province

---
+++ API/connect/src/main/java/com/learn/first/restapi/regions/controller/CProvinceController.java
@@ -0,0 +1,42 @@
+package com.learn.first.restapi.regions.controller;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.first.restapi.regions.model.CProvince;
+import com.learn.first.restapi.regions.repository.IProvinceRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/")
+public class CProvinceController {
+
+    @Autowired
+    IProvinceRepository pIProvinceRepository;
+
+    @GetMapping("/provinces")
+    public ResponseEntity<List<CProvince>> getAllProvinces() {
+
+        try {
+            List<CProvince> pProvinces = new ArrayList<CProvince>();
+
+            pIProvinceRepository.findAll().forEach(pProvinces::add);
+
+            return new ResponseEntity<>(pProvinces, HttpStatus.OK);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
