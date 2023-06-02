7d5e8c9e8e81f50b29e2dcabb650f865b9e9c270
Your Name <n.t.quoctoan@gmail.com>
2023-06-02 20:24:54 +0700

    update handle error

--- API/connect/src/main/java/com/learn/first/restapi/error/CustomGlobalExceptionHandler.java
+++ API/connect/src/main/java/com/learn/first/restapi/error/CustomGlobalExceptionHandler.java
@@ -6,13 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
