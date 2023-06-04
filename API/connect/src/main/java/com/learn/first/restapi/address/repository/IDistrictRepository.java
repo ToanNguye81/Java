bfe04e2902975e53b7371dab7742fd17d415fe6c
Your Name <n.t.quoctoan@gmail.com>
2023-06-04 20:15:08 +0700

    crud district

--- API/connect/src/main/java/com/learn/first/restapi/address/repository/IDistrictRepository.java
+++ API/connect/src/main/java/com/learn/first/restapi/address/repository/IDistrictRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.learn.first.restapi.address.model.CDistrict;
 
 public interface IDistrictRepository extends JpaRepository<CDistrict, Long> {
-    // find district by id
-    CDistrict findById(int id);
-
     // find district by Name
     CDistrict findByName(String districtName);
 
