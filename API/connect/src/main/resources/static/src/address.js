7e1116fdf37bad0703994bbf9bb4bf9597964425
Your Name <n.t.quoctoan@gmail.com>
2023-06-06 23:26:55 +0700

    update address interface

--- API/connect/src/main/resources/static/src/address.js
+++ API/connect/src/main/resources/static/src/address.js
@@ -1,49 +1,80 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", onPageLoading);
 
-function loadDistricts() {
-  var selectedProvince = document.getElementById("province").value;
+// refresh - on page loading
+function onPageLoading() {
+  loadAllProvince();
+  loadAllDistrict();
+}
 
+// Load district by province code
+function loadDistrictByProvinceCode() {
+  var selectedProvince = document.getElementById("province").value;
   var districtSelect = document.getElementById("district");
   districtSelect.innerHTML = '<option value="">Chọn Quận/huyện</option>';
-  fetch("http://127.0.0.1:8080/districts?provinceCode=" + selectedProvince)
+  fetch("/district?provinceCode=" + selectedProvince)
     .then((response) => response.json())
     .then((data) => {
       data.forEach((district) => {
         var option = document.createElement("option");
         option.value = district.id;
-        option.innerHTML = district.name;
+        option.innerHTML = district.prefix + " " + district.name;
         districtSelect.appendChild(option);
       });
     });
 }
 
-function loadWards() {
+// Load ward by district id
+function loadWardByDistrictId() {
   var selectedDistrict = document.getElementById("district").value;
   var wardSelect = document.getElementById("ward");
   wardSelect.innerHTML = '<option value="">Chọn xã/phường</option>';
-  fetch("http://127.0.0.1:8080/wards?districtId=" + selectedDistrict)
+  fetch("/ward?districtId=" + selectedDistrict)
     .then((response) => response.json())
     .then((data) => {
       data.forEach((ward) => {
         var option = document.createElement("option");
         option.value = ward.id;
-        option.innerHTML = ward.name;
+        option.innerHTML = ward.prefix + " " + ward.name;
         wardSelect.appendChild(option);
       });
     });
 }
 
-function onPageLoading() {
+//load all province
+function loadAllProvince() {
   var provinceSelect = document.getElementById("province");
-  fetch("http://127.0.0.1:8080/provinces")
+  var provinceForDistrictSelect = document.getElementById(
+    "create-province-for-district"
+  );
+  fetch("/province/all")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((province) => {
-        var option = document.createElement("option");
-        option.value = province.code;
-        option.innerHTML = province.name;
-        provinceSelect.appendChild(option);
+        var option1 = document.createElement("option");
+        option1.value = province.code;
+        option1.innerHTML = province.name;
+        provinceSelect.appendChild(option1);
+
+        var option2 = document.createElement("option");
+        option2.value = province.code;
+        option2.innerHTML = province.name;
+        provinceForDistrictSelect.appendChild(option2);
+      });
+    });
+}
+
+//load all district
+function loadAllDistrict() {
+  var districtSelect = document.getElementById("create-district-for-ward");
+  fetch("/district/all")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((province) => {
+        var option1 = document.createElement("option");
+        option1.value = province.code;
+        option1.innerHTML = province.name;
+        districtSelect.appendChild(option1);
       });
     });
 }
