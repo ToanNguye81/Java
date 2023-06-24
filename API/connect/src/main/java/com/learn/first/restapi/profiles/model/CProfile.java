0462aaec7357a3a7e692b73024793ccba625ec29
Your Name <n.t.quoctoan@gmail.com>
2023-06-24 18:46:44 +0700

    update user CProfile.jv

--- API/connect/src/main/java/com/learn/first/restapi/profiles/model/CProfile.java
+++ API/connect/src/main/java/com/learn/first/restapi/profiles/model/CProfile.java
@@ -1,7 +1,6 @@
 package com.learn.first.restapi.profiles.model;
 
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -20,9 +19,12 @@ public class CProfile {
     private long id;
 
     @Column(name = "phone")
+    @NotNull(message = "Vui lòng nhập số điện thoại")
+    @Size(min = 10, max = 10, message = "Số điện thoại phải có đúng 10 kí tự")
     private String phone;
 
-    @Column(name = "")
+    @Column(name = "gender")
+    @NotNull(message = "Giới tính không được để trống")
     private String gender;
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -30,11 +32,14 @@ public class CProfile {
     @JsonFormat(pattern = "dd-MM-yyyy")
     private Date dateOfBirth;
 
-    @Column(name = "")
+    @Column(name = "address")
+    @NotNull(message = "Địa chỉ không được để trống")
     private String address;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
+    // Khai báo kiểu quan hệ 1-1: user-profile
+    @OneToOne(fetch = FetchType.LAZY, optional = false) // data sẽ chỉ được tải khi nó được truy cập hoặc yêu cầu.
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private CUser user;
 
     public CProfile() {
