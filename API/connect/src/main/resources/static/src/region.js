20da415440e73b3cebaf362482172e2898a5be5f
Your Name <n.t.quoctoan@gmail.com>
2023-06-26 22:16:49 +0700

    update router region.js

--- API/connect/src/main/resources/static/src/region.js
+++ API/connect/src/main/resources/static/src/region.js
@@ -1,22 +1,22 @@
-'use strict';
-$.get(`http://127.0.0.1:8080/api/countries`, getCountry);
-$.get(`http://127.0.0.1:8080/api/countries/regions`, loadRegionToTable);
-let vCountrySelectElement = $('#select-country');
+"use strict";
+$.get(`/countries`, getCountry);
+$.get(`/regions`, loadRegionToTable);
+let vCountrySelectElement = $("#select-country");
 function getCountry(paramCountry) {
   paramCountry.forEach((country) => {
-    $('<option>', {
+    $("<option>", {
       text: country.countryName,
-      value: country.countryId,
+      value: country.id,
     }).appendTo(vCountrySelectElement);
   });
 }
 
-let regionTable = $('#region-table').DataTable({
+let regionTable = $("#region-table").DataTable({
   columns: [
-    { data: 'regionId' },
-    { data: 'regionCode' },
-    { data: 'regionName' },
-    { data: 'action' },
+    { data: "id" },
+    { data: "regionCode" },
+    { data: "regionName" },
+    { data: "action" },
   ],
   columnDefs: [
     {
@@ -37,13 +37,11 @@ vCountrySelectElement.change(onGetRegionChange);
 
 function onGetRegionChange(event) {
   gCountryId = event.target.value;
+  console.log(gCountryId);
   if (gCountryId == 0) {
-    $.get(`http://127.0.0.1:8080/api/countries/regions`, loadRegionToTable);
+    $.get(`/regions`, loadRegionToTable);
   } else {
-    $.get(
-      `http://127.0.0.1:8080/api/countries/${gCountryId}/regions`,
-      loadRegionToTable,
-    );
+    $.get(`/countries/${gCountryId}/regions`, loadRegionToTable);
   }
 }
 
@@ -53,29 +51,26 @@ let gRegionId = 0;
 
 let region = {
   newRegion: {
-    regionCode: '',
-    regionName: '',
+    regionCode: "",
+    regionName: "",
   },
   onCreateRegionClick() {
     this.newRegion = {
-      regionCode: $('#input-region-Code').val().trim(),
-      regionName: $('#input-region-name').val().trim(),
+      regionCode: $("#input-region-Code").val().trim(),
+      regionName: $("#input-region-name").val().trim(),
     };
     gRegionId = 0;
     if (validateRegion(this.newRegion)) {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/${gCountryId}/regions`,
-        method: 'POST',
-        contentType: 'application/json',
+        url: `/countries/${gCountryId}/regions`,
+        method: "POST",
+        contentType: "application/json",
         data: JSON.stringify(this.newRegion),
         success: (region) => {
           alert(
-            `Đã tạo thành công region có  code là: ${region.regionCode} và tên là: ${region.regionName}`,
+            `Đã tạo thành công region có  code là: ${region.regionCode} và tên là: ${region.regionName}`
           );
-          $.get(
-            `http://127.0.0.1:8080/api/countries/${gCountryId}/regions`,
-            loadRegionToTable,
-          );
+          $.get(`/countries/${gCountryId}/regions`, loadRegionToTable);
           resetRegionInput();
         },
         error: (err) => alert(err.responseText),
@@ -83,33 +78,27 @@ let region = {
     }
   },
   onEditRegionClick() {
-    let vSelectedRow = $(this).parents('tr');
+    let vSelectedRow = $(this).parents("tr");
     let vSelectedData = regionTable.row(vSelectedRow).data();
-    gRegionId = vSelectedData.regionId;
-    $.get(
-      `http://127.0.0.1:8080/api/countries/regions/${gRegionId}`,
-      loadRegionToInput,
-    );
+    gRegionId = vSelectedData.id;
+    $.get(`/regions/${gRegionId}`, loadRegionToInput);
   },
   onUpdateRegionClick() {
     this.newRegion = {
-      regionCode: $('#input-region-Code').val().trim(),
-      regionName: $('#input-region-name').val().trim(),
+      regionCode: $("#input-region-Code").val().trim(),
+      regionName: $("#input-region-name").val().trim(),
     };
     if (validateRegion(this.newRegion)) {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/regions/${gRegionId}`,
-        method: 'PUT',
-        contentType: 'application/json',
+        url: `/regions/${gRegionId}`,
+        method: "PUT",
+        contentType: "application/json",
         data: JSON.stringify(this.newRegion),
         success: (region) => {
           alert(
-            `Đã update thành công region có  code là: ${region.regionCode} và tên là: ${region.regionName}`,
+            `Đã update thành công region có  code là: ${region.regionCode} và tên là: ${region.regionName}`
           );
-          $.get(
-            `http://127.0.0.1:8080/api/countries/${gCountryId}/regions`,
-            loadRegionToTable,
-          );
+          $.get(`/countries/${gCountryId}/regions`, loadRegionToTable);
           resetRegionInput();
         },
         error: (err) => alert(err.responseText),
@@ -117,41 +106,35 @@ let region = {
     }
   },
   onDeleRegionByIdClick() {
-    $('#modal-delete-region').modal('show');
-    let vSelectedRow = $(this).parents('tr');
+    $("#modal-delete-region").modal("show");
+    let vSelectedRow = $(this).parents("tr");
     let vSelectedData = regionTable.row(vSelectedRow).data();
-    gRegionId = vSelectedData.regionId;
+    gRegionId = vSelectedData.id;
   },
   onDeleteAllRegionClick() {
-    $('#modal-delete-region').modal('show');
+    $("#modal-delete-region").modal("show");
     gRegionId = 0;
   },
   onConfirmDeleteRegionClick() {
     if (gRegionId == 0) {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/regions`,
-        method: 'DELETE',
+        url: `/regions`,
+        method: "DELETE",
         success: () => {
           alert(`All Region were successfully Delete`);
-          $('#modal-delete-region').modal('hide');
-          $.get(
-            `http://127.0.0.1:8080/api/countries/regions`,
-            loadRegionToTable,
-          );
+          $("#modal-delete-region").modal("hide");
+          $.get(`/regions`, loadRegionToTable);
         },
         error: (err) => alert(err.responseText),
       });
     } else {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/regions/${gRegionId}`,
-        method: 'DELETE',
+        url: `/regions/${gRegionId}`,
+        method: "DELETE",
         success: () => {
           alert(`Region with id: ${gRegionId} were successfully Delete`);
-          $('#modal-delete-region').modal('hide');
-          $.get(
-            `http://127.0.0.1:8080/api/countries/regions`,
-            loadRegionToTable,
-          );
+          $("#modal-delete-region").modal("hide");
+          $.get(`/regions`, loadRegionToTable);
         },
         error: (err) => alert(err.responseText),
       });
@@ -159,16 +142,16 @@ let region = {
   },
 };
 
-$('#create-region').click(region.onCreateRegionClick);
-$('#update-region').click(region.onUpdateRegionClick);
-$('#delete-all-region').click(region.onDeleteAllRegionClick);
-$('#delete-region').click(region.onConfirmDeleteRegionClick);
-$('#region-table').on('click', '.fa-edit', region.onEditRegionClick);
-$('#region-table').on('click', '.fa-trash', region.onDeleRegionByIdClick);
+$("#create-region").click(region.onCreateRegionClick);
+$("#update-region").click(region.onUpdateRegionClick);
+$("#delete-all-region").click(region.onDeleteAllRegionClick);
+$("#delete-region").click(region.onConfirmDeleteRegionClick);
+$("#region-table").on("click", ".fa-edit", region.onEditRegionClick);
+$("#region-table").on("click", ".fa-trash", region.onDeleRegionByIdClick);
 
 function loadRegionToInput(paramRegion) {
-  $('#input-region-Code').val(paramRegion.regionCode);
-  $('#input-region-name').val(paramRegion.regionName);
+  $("#input-region-Code").val(paramRegion.regionCode);
+  $("#input-region-name").val(paramRegion.regionName);
 }
 
 function validateRegion(paramRegion) {
@@ -176,15 +159,15 @@ function validateRegion(paramRegion) {
   try {
     if (gCountryId == 0) {
       vResult = false;
-      throw 'xin hãy chọn 1 đất nước';
+      throw "xin hãy chọn 1 đất nước";
     }
-    if (paramRegion.regionCode == '') {
+    if (paramRegion.regionCode == "") {
       vResult = false;
-      throw 'Xin hãy cho biết Mã Vùng Miền';
+      throw "Xin hãy cho biết Mã Vùng Miền";
     }
-    if (paramRegion.regionName == '') {
+    if (paramRegion.regionName == "") {
       vResult = false;
-      throw 'Xin hãy cho biết tên Vùng Miền';
+      throw "Xin hãy cho biết tên Vùng Miền";
     }
   } catch (e) {
     alert(e);
@@ -193,45 +176,42 @@ function validateRegion(paramRegion) {
 }
 
 function resetRegionInput() {
-  $('#input-region-Code').val('');
-  $('#input-region-name').val('');
+  $("#input-region-Code").val("");
+  $("#input-region-name").val("");
 }
 // country
 let country = {
   newCountry: {
-    countryCode: '',
-    countryName: '',
+    countryCode: "",
+    countryName: "",
   },
   onNewCountryClick() {
-    $('#modal-create-country').modal('show');
+    $("#modal-create-country").modal("show");
     gCountryId = 0;
   },
   onUpdateCountryClick() {
     if (gCountryId != 0) {
-      $('#modal-create-country').modal('show');
-      $.get(
-        `http://127.0.0.1:8080/api/countries/${gCountryId}`,
-        loadCountryToInput,
-      );
+      $("#modal-create-country").modal("show");
+      $.get(`/countries/${gCountryId}`, loadCountryToInput);
     } else {
-      alert('Please select country to update');
+      alert("Please select country to update");
     }
   },
   onSaveCountryClick() {
     this.newCountry = {
-      countryCode: $('#input-country-code').val(),
-      countryName: $('#input-country-name').val(),
+      countryCode: $("#input-country-code").val(),
+      countryName: $("#input-country-name").val(),
     };
     if (gCountryId == 0) {
       if (validateCountry(this.newCountry)) {
         $.ajax({
-          url: 'http://127.0.0.1:8080/api/countries',
-          method: 'POST',
+          url: "/countries",
+          method: "POST",
           data: JSON.stringify(this.newCountry),
-          contentType: 'application/json',
+          contentType: "application/json",
           success: (country) => {
             alert(`Đã tạo thành công country`);
-            $('#modal-create-country').modal('hide');
+            $("#modal-create-country").modal("hide");
             resetCountryInput();
             location.reload();
           },
@@ -241,13 +221,13 @@ let country = {
     } else {
       if (validateCountry(this.newCountry)) {
         $.ajax({
-          url: `http://127.0.0.1:8080/api/countries/${gCountryId}`,
-          method: 'PUT',
+          url: `/countries/${gCountryId}`,
+          method: "PUT",
           data: JSON.stringify(this.newCountry),
-          contentType: 'application/json',
+          contentType: "application/json",
           success: (country) => {
             alert(`Đã cập nhât thành công country với id: ${gCountryId}`);
-            $('#modal-create-country').modal('hide');
+            $("#modal-create-country").modal("hide");
             resetCountryInput();
             location.reload();
           },
@@ -258,20 +238,20 @@ let country = {
   },
   onDeleteCountryClick() {
     if (gCountryId != 0) {
-      $('#modal-delete-country').modal('show');
+      $("#modal-delete-country").modal("show");
     } else {
-      alert('Please select country to delete');
+      alert("Please select country to delete");
     }
   },
   onDeleteAllCountryClick() {
-    $('#modal-delete-country').modal('show');
+    $("#modal-delete-country").modal("show");
     gCountryId == 0;
   },
   onConfirmDeleteCountryClick() {
     if (gCountryId == 0) {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/`,
-        method: 'DELETE',
+        url: `/countries/`,
+        method: "DELETE",
         success: () => {
           alert(`Successfully Delete All Country`);
           location.reload();
@@ -280,8 +260,8 @@ let country = {
       });
     } else {
       $.ajax({
-        url: `http://127.0.0.1:8080/api/countries/${gCountryId}`,
-        method: 'DELETE',
+        url: `/countries/${gCountryId}`,
+        method: "DELETE",
         success: () => {
           alert(`Successfully Delete Country with id: ${gCountryId}`);
           location.reload();
@@ -292,24 +272,24 @@ let country = {
   },
 };
 
-$('#create-country').click(country.onNewCountryClick);
-$('#update-country').click(country.onUpdateCountryClick);
-$('#btn-save-country').click(country.onSaveCountryClick);
-$('#delete-country').click(country.onDeleteCountryClick);
-$('#delete-all-country').click(country.onDeleteAllCountryClick);
-$('#btn-delete-country').click(country.onConfirmDeleteCountryClick);
+$("#create-country").click(country.onNewCountryClick);
+$("#update-country").click(country.onUpdateCountryClick);
+$("#btn-save-country").click(country.onSaveCountryClick);
+$("#delete-country").click(country.onDeleteCountryClick);
+$("#delete-all-country").click(country.onDeleteAllCountryClick);
+$("#btn-delete-country").click(country.onConfirmDeleteCountryClick);
 
 function validateCountry(paramCountry) {
   let vResult = true;
   try {
-    if (paramCountry.countryCode == '') {
+    if (paramCountry.countryCode == "") {
       vResult = false;
-      throw 'Không được để trống country code';
+      throw "Không được để trống country code";
     }
 
-    if (paramCountry.countryName == '') {
+    if (paramCountry.countryName == "") {
       vResult = false;
-      throw 'Không được để trống country name';
+      throw "Không được để trống country name";
     }
   } catch (e) {
     alert(e);
@@ -318,11 +298,11 @@ function validateCountry(paramCountry) {
 }
 
 function loadCountryToInput(paramCountry) {
-  $('#input-country-code').val(paramCountry.countryCode);
-  $('#input-country-name').val(paramCountry.countryName);
+  $("#input-country-code").val(paramCountry.countryCode);
+  $("#input-country-name").val(paramCountry.countryName);
 }
 
 function resetCountryInput() {
-  $('#input-country-code').val('');
-  $('#input-country-name').val('');
+  $("#input-country-code").val("");
+  $("#input-country-name").val("");
 }
